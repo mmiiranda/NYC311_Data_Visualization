816a9f7e05c9f031c960f1c045237e3b43fcb97b
mmiiranda <mauricio201190b@gmail.com>
2025-03-01 22:58:33 -0300

    refactor: turning fetchData into a func

--- fetchData.py
+++ fetchData.py
@@ -1,31 +1,70 @@
-# This code make a HTTPS request from Cityofnewyork API 
 import requests
 import pandas as pd
+from urllib.parse import urlencode
 
-# Variables of Search
-offset=0
-limit = 10
-
-data = []
-
-while offset < limit:
-    # City Of New York API then accepts 1000 requests at a time, this logic solves this problem
-    aux = min(1000, limit - offset)
+def fetch_nyc311_data(
+    total_records=2000,
+    complaint_type=None,
+    borough=None,
+    start_date=None,
+    end_date=None,
+    select_cols=None
+):
     
-    url = f"https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit={aux}&$offset={offset}"
-    response = requests.get(url)
+    base_url = "https://data.cityofnewyork.us/resource/erm2-nwe9.json"
+    fetched_data = []
     
-    if response.status_code == 200:
-        chunk_data = response.json()  
-        data.extend(chunk_data)
-    else:
-        print(f"Erro na requisição: {response.status_code}")
-
+    where_clauses = []
     
-    offset += aux
-
-print(data[0])
-
-# Transform in CSV file and download
-df = pd.DataFrame(data)
-df.to_csv("nyc311_data.csv", index=False)
+    if complaint_type:
+        where_clauses.append(f"complaint_type = '{complaint_type}'")
+    if borough:
+        where_clauses.append(f"borough = '{borough.upper()}'")
+    if start_date and end_date:
+        where_clauses.append(
+            f"created_date >= '{start_date}T00:00:00' AND created_date <= '{end_date}T23:59:59'"
+        )
+    elif start_date:
+        where_clauses.append(f"created_date >= '{start_date}T00:00:00'")
+    elif end_date:
+        where_clauses.append(f"created_date <= '{end_date}T23:59:59'")
+    
+    where_query = " AND ".join(where_clauses) if where_clauses else None
+    
+    select_query = ", ".join(select_cols) if select_cols else None
+    
+    offset = 0
+    batch_size = 1000
+    registros_buscados = 0
+    
+    while registros_buscados < total_records:
+        remaining = total_records - registros_buscados
+        limit = min(batch_size, remaining)
+        
+        params = {
+            "$limit": limit,
+            "$offset": offset
+        }
+        
+        if where_query:
+            params["$where"] = where_query
+        if select_query:
+            params["$select"] = select_query
+        
+        query_string = urlencode(params)
+        url = f"{base_url}?{query_string}"
+        
+        response = requests.get(url)
+        if response.status_code == 200:
+            chunk_data = response.json()
+            if len(chunk_data) == 0:
+                break
+            fetched_data.extend(chunk_data)
+            registros_buscados += len(chunk_data)
+            offset += len(chunk_data)
+        else:
+            print(f"Erro na requisição (status {response.status_code}): {response.text}")
+            break
+    
+    df = pd.DataFrame(fetched_data)
+    return df
